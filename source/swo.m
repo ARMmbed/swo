6b3b1fe9c40864cb313d16c9db3328c1c2a371cb
James Crosby <James.Crosby@arm.com>
2013-11-15 14:19:07 +0000

    swo api: exploring generic-package-defers-to-specialised-package-for-implementation approach

---
+++ source/swo.m
@@ -0,0 +1,30 @@
+/// Copyright (C) 2013 ARM Limited, All rights reserved.
+
+#import "swo/swo.h"
+
+#ifdef EFM32GG
+
+// trying out a specific-module-provides-standard-interface type approach with
+// this:
+#import "swo-platform/swo-platform.h"
+
+void swoInit(){
+    // nothing to do: as SWO has to be accessible everywhere, including ISRs,
+    // we can't easily communicate the dependency on clocks etc. to other
+    // components - so every time a character is sent over SWO we check that
+    // things are configured & switched on as they need to be, and if not
+    // set them up again
+}
+
+void swoSendChar(char c){
+    swoPlatformEnsureInit();
+    swoPlatformSendChar(c);
+}
+
+#else // def EFM32GG
+
+#error "no supported platform detected" // FIXME this should be a configure-time error
+
+#endif // else def EFM32GG
+
+
---
+++ swo/swo.h
@@ -0,0 +1,8 @@
+/// Copyright (C) 2013 ARM Limited, All rights reserved.
+
+/// @name Single Wire Output API
+
+void swoInit();
+void swoPutChar(char c);
+
+
