d55a2f65daa36d1a8c28fa7a9dfa1cdbed71c054
Marcus Chang <marcus.chang@arm.com>
2015-05-21 17:29:10 +0100

    Revised module. Added printf.

--- source/swo.c
+++ source/swo.c
@@ -1,30 +1,59 @@
-/// Copyright (C) 2013 ARM Limited, All rights reserved.
+/* mbed
+ * Copyright (c) 2006-2015 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "swo/swo.h"
-
-#ifdef EFM32GG
-
-// trying out a specific-module-provides-standard-interface type approach with
-// this:
 #include "swo-platform/swo-platform.h"
 
-void swoInit(){
-    // nothing to do: as SWO has to be accessible everywhere, including ISRs,
-    // we can't easily communicate the dependency on clocks etc. to other
-    // components - so every time a character is sent over SWO we check that
-    // things are configured & switched on as they need to be, and if not
-    // set them up again
+#include <stdio.h>
+#include <stdarg.h>
+
+
+void swoInit()
+{
+    swoPlatformEnsureInit();
 }
 
-void swoSendChar(char c){
+void swoSendChar(char c)
+{
     swoPlatformEnsureInit();
     swoPlatformSendChar(c);
 }
 
-#else // def EFM32GG
+static void swoSend(const char* buffer, int length)
+{
+    swoPlatformEnsureInit();
 
-#error "no supported platform detected" // FIXME this should be a configure-time error
+    for (int idx = 0; idx < length; idx++)
+    {
+        swoPlatformSendChar(buffer[idx]);
+    }
+}
 
-#endif // else def EFM32GG
+int swoprintf(const char* format, ...)
+{
+    char buffer[100] = {0};
 
+    va_list arg;
+    va_start(arg, format);
+
+    int r = vsnprintf(buffer, 100, format, arg);
+    va_end(arg);
+
+    swoSend(buffer, r);
+
+    return r;
+}
 
